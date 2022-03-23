70b01aa2f72506d080980ff7f02723acd1b05ab7
rangga <aryashenarangga@gmail.com>
2022-03-23 21:29:31 +0700

    inisialisasi project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+            taxi.DriverName = "Martin";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 5;
+            taxi.TaxiInfo();
+            Console.WriteLine();
+            taxi.PickupPassenger();
+            taxi.DropOffPassenger();
+            Console.ReadKey();
+        }
+    }
+}
